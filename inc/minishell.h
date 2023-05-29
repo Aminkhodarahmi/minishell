6408e36fe1e24dedfa9592e08578ae7e81bc9c01
Rouven Nauke <rnauke@2-I-3.42heilbronn.de>
2023-05-29 17:39:56 +0200

    header file

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,4 +1,33 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnauke <rnauke@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/12 20:12:09 by rnauke            #+#    #+#             */
+/*   Updated: 2023/05/29 17:39:20 by rnauke           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <term.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <curses.h>
+# include <unistd.h>
+# include <signal.h>
+# include <dirent.h>
+# include <termios.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+
+
+
+
 
 // structs
 typedef struct s_shell
@@ -6,3 +35,5 @@ typedef struct s_shell
 	
 }	t_shell;
 // function
+
+#endif
