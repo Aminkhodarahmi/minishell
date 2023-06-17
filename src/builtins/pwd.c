0a5b63a24bc7485c05260c30c3a597a95fff415e
Rouven Nauke <rnauke@2-E-1.42heilbronn.de>
2023-06-17 17:46:14 +0200

    pwd

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -3,10 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akhodara <akhodara@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rnauke <rnauke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 19:28:00 by akhodara          #+#    #+#             */
-/*   Updated: 2023/06/09 19:28:01 by akhodara         ###   ########.fr       */
+/*   Updated: 2023/06/17 17:45:43 by rnauke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../inc/minishell.h"
+
+void	print_pwd(void)
+{
+	char	*s;
+
+	s = NULL;
+	s = getcwd(s, 256);
+	ft_printf("%s\n", s);
+	free(s);
+	}
